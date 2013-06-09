fe697fee9c4a977565402ae418fe0bac7a6c8848
Oskar Jarczyk <oskar.jarczyk@gmail.com>
2013-06-09 02:25:00 +0200

    small

--- src/EmergingTeams/Node.java
+++ src/EmergingTeams/Node.java
@@ -1,6 +1,4 @@
-
 package EmergingTeams;
-
 
 import java.io.*;
 import java.math.*;
@@ -42,14 +40,13 @@ import static java.lang.Math.*;
 import static repast.simphony.essentials.RepastEssentials.*;
 
 /**
- *
- * This is an agent.
- * A person, a contributor to the COIN Network
+ * 
+ * This is an agent. A person, a contributor to the COIN Network
  * 
  * @author Oskar Jarczyk
- *
+ * 
  */
-public class Node  {
+public class Node {
 
 	public int msgSent = 0;
 	public ArrayList<Message> msgReceived;
@@ -58,110 +55,135 @@ public class Node  {
 	public double energy = 0;
 	public ArrayList<Message> msgToSendList;
 
+	@Parameter(displayName = "Msg Sent", usageName = "msgSent")
+	public int getMsgSent() {
+		return msgSent;
+	}
 
-	@Parameter (displayName = "Msg Sent", usageName = "msgSent")
-	public int getMsgSent() { return msgSent; }
-	public void setMsgSent(int newValue) { msgSent = newValue; }
+	public void setMsgSent(int newValue) {
+		msgSent = newValue;
+	}
 
+	@Parameter(displayName = "Msg Received list", usageName = "msgReceived")
+	public ArrayList<Message> getMsgReceived() {
+		return msgReceived;
+	}
 
-	@Parameter (displayName = "Msg Received list", usageName = "msgReceived")
-	public ArrayList<Message> getMsgReceived() { return msgReceived; }
-	public void setMsgReceived(ArrayList<Message> newValue) { msgReceived = newValue; }
+	public void setMsgReceived(ArrayList<Message> newValue) {
+		msgReceived = newValue;
+	}
 
+	@Parameter(displayName = "Global Messenger", usageName = "globalMessenger")
+	public GlobalMessenger getGlobalMessenger() {
+		return globalMessenger;
+	}
 
-	@Parameter (displayName = "Global Messenger", usageName = "globalMessenger")
-	public GlobalMessenger getGlobalMessenger() { return globalMessenger; }
-	public void setGlobalMessenger(GlobalMessenger newValue) { globalMessenger = newValue; }
+	public void setGlobalMessenger(GlobalMessenger newValue) {
+		globalMessenger = newValue;
+	}
 
+	@Parameter(displayName = "Comm Range", usageName = "communicationRange")
+	public double getCommunicationRange() {
+		return communicationRange;
+	}
 
-	@Parameter (displayName = "Comm Range", usageName = "communicationRange")
-	public double getCommunicationRange() { return communicationRange;  }
-	public void setCommunicationRange(double newValue) { communicationRange = newValue; }
+	public void setCommunicationRange(double newValue) {
+		communicationRange = newValue;
+	}
 
+	@Parameter(displayName = "Energy", usageName = "energy")
+	public double getEnergy() {
+		return energy;
+	}
 
-	@Parameter (displayName = "Energy", usageName = "energy")
-	public double getEnergy() { return energy; }
-	public void setEnergy(double newValue) { energy = newValue; }
+	public void setEnergy(double newValue) {
+		energy = newValue;
+	}
 
-	@Parameter (displayName = "Msg To Send List", usageName = "msgToSendList")
-	public ArrayList<Message> getMsgToSendList() { return msgToSendList; }
-	public void setMsgToSendList(ArrayList<Message> newValue) { msgToSendList = newValue; }
+	@Parameter(displayName = "Msg To Send List", usageName = "msgToSendList")
+	public ArrayList<Message> getMsgToSendList() {
+		return msgToSendList;
+	}
 
+	public void setMsgToSendList(ArrayList<Message> newValue) {
+		msgToSendList = newValue;
+	}
 
 	/**
-	 *
-	 * The serialization runtime associates with each serializable class 
-	 * a version number, called a serialVersionUID, which is used during 
-	 * deserialization to verify that the sender and receiver of a serialized 
-	 * object have loaded classes for that object 
-	 * that are compatible with respect to serialization.
+	 * 
+	 * The serialization runtime associates with each serializable class a
+	 * version number, called a serialVersionUID, which is used during
+	 * deserialization to verify that the sender and receiver of a serialized
+	 * object have loaded classes for that object that are compatible with
+	 * respect to serialization.
+	 * 
 	 * @field serialVersionUID
-	 *
+	 * 
 	 */
 	private static final long serialVersionUID = 42L;
 	// answer to the life, universe and everything..
 
 	/**
-	 *
+	 * 
 	 * This value is used to automatically generate agent identifiers.
+	 * 
 	 * @field agentIDCounter
-	 *
+	 * 
 	 */
 	protected static long agentIDCounter = 1;
 
 	/**
-	 *
+	 * 
 	 * This value is the agent's identifier.
+	 * 
 	 * @field agentID
-	 *
+	 * 
 	 */
 	protected long numericID = agentIDCounter;
 	protected String agentID = "Node " + (agentIDCounter++);
 
-
-
 	/**
-	 *
+	 * 
 	 * This is the step behavior.
+	 * 
 	 * @method initialize
-	 *
+	 * 
 	 */
 	public void initialize(GlobalMessenger aGlobalMessenger) {
 
 		setMsgReceived(new ArrayList<Message>());
 		setMsgSent(0);
 		setGlobalMessenger(aGlobalMessenger);
-		setEnergy(RandomDraw(1,100));
+		setEnergy(RandomDraw(1, 100));
 		setMsgToSendList(new ArrayList<Message>());
 
 	}
 
-
 	/**
-	 *
-	 * @method updateNeighbors
-	 * This method updates the SensorNetwork neighbors
-	 * and it runs at the start of each timestep (Priority = FIRST).
-	 * This is useful in case the nodes are moving. 
-	 *
+	 * 
+	 * @method updateNeighbors This method updates the SensorNetwork neighbors
+	 *         and it runs at the start of each timestep (Priority = FIRST).
+	 *         This is useful in case the nodes are moving.
+	 * 
 	 */
-	@ScheduledMethod(
-			start = 1d,
-			priority = 1.7976931348623157E308d, //priority = FIRST
-			shuffle = false
-	)
+	@ScheduledMethod(start = 1d, priority = 1.7976931348623157E308d, // priority
+																		// =
+																		// FIRST
+	shuffle = false)
 	public void updateNeighbors() {
 
-		Network network = (Network)FindProjection("GlobalMessenger/SensorNetwork");
-		Iterator netNeighbors = new NetworkAdjacent(network, this).query().iterator();
+		Network network = (Network) FindProjection("GlobalMessenger/SensorNetwork");
+		Iterator netNeighbors = new NetworkAdjacent(network, this).query()
+				.iterator();
 
 		while (netNeighbors.hasNext()) {
 			Object oldNeighbor = netNeighbors.next();
 			RemoveEdge("GlobalMessenger/SensorNetwork", this, oldNeighbor);
 		}
 
-		ContinuousSpace mySpace = (ContinuousSpace)FindProjection("GlobalMessenger/ContinuousSpace2D");
-		Iterator list = new ContinuousWithin(mySpace, this, communicationRange).query().iterator();
+		ContinuousSpace mySpace = (ContinuousSpace) FindProjection("GlobalMessenger/ContinuousSpace2D");
+		Iterator list = new ContinuousWithin(mySpace, this, communicationRange)
+				.query().iterator();
 		double myX = mySpace.getLocation(this).getX();
 		double myY = mySpace.getLocation(this).getY();
 
@@ -171,41 +193,33 @@ public class Node  {
 
 			if (o instanceof Node) {
 				CreateEdge("GlobalMessenger/SensorNetwork", this, o, 1.0);
-			} 
+			}
 		}
-
 
 	}
 
-
-
 	/**
-	 *
+	 * 
 	 * This is the step behavior.
-	 * @method step
-	 * Note that each node HAS TO execute the operations in this order compulsory:
-	 * 1) RECEIVE msgs
-	 * 2) PROCESS msgs
-	 * 3) SEND msgs	 
-	 *  
-	 * (Otherwise you screw up the simulation given the design of the GlobalMessenger)
-	 *
+	 * 
+	 * @method step Note that each node HAS TO execute the operations in this
+	 *         order compulsory: 1) RECEIVE msgs 2) PROCESS msgs 3) SEND msgs
+	 * 
+	 *         (Otherwise you screw up the simulation given the design of the
+	 *         GlobalMessenger)
+	 * 
 	 */
-	@ScheduledMethod(
-			start = 1d,
-			interval = 1d,
-			priority = 0, // NORMAL priority 
-			shuffle = false
-	)	
+	@ScheduledMethod(start = 1d, interval = 1d, priority = 0, // NORMAL priority
+	shuffle = false)
 	public void step() {
 
-		// RECEIVE msgs 
+		// RECEIVE msgs
 		ArrayList<Message> msgReceivedTmp = globalMessenger.receive(this);
 
 		// PROCESS msgs
 		if (msgReceivedTmp.size() > 0) {
 			msgReceived.addAll(msgReceivedTmp);
-		}        
+		}
 		setMsgToSendList(hello());
 
 		for (int i = 0; i < msgToSendList.size(); i++) {
@@ -217,16 +231,15 @@ public class Node  {
 		setMsgToSendList(new ArrayList<Message>());
 	}
 
-
-
 	/**
-	 *
+	 * 
 	 * This is the step behavior.
+	 * 
 	 * @method hello
-	 *
-	 * Process received messages and 
-	 * returns an ArrayList of Message to send.
-	 *
+	 * 
+	 *         Process received messages and returns an ArrayList of Message to
+	 *         send.
+	 * 
 	 */
 	public ArrayList<Message> hello() {
 
@@ -237,24 +250,31 @@ public class Node  {
 
 			for (int i = 0; i < msgReceived.size(); i++) {
 
-				Message aMsgReceived = (Message)msgReceived.get(i);
-				returnValue.add(new Message(this, aMsgReceived.getSender(), new String("Hello back from "+this+ " to " + aMsgReceived.getSender()), timeDouble));
-				System.out.println(GetTickCount() + " - "+ this+" RECEIVED from "+ aMsgReceived.getSender() +": "+(String)aMsgReceived.getContent() + " (timestamp: "+aMsgReceived.getTimestamp()+")");
+				Message aMsgReceived = (Message) msgReceived.get(i);
+				returnValue.add(new Message(this, aMsgReceived.getSender(),
+						new String("Hello back from " + this + " to "
+								+ aMsgReceived.getSender()), timeDouble));
+				System.out.println(GetTickCount() + " - " + this
+						+ " RECEIVED from " + aMsgReceived.getSender() + ": "
+						+ (String) aMsgReceived.getContent() + " (timestamp: "
+						+ aMsgReceived.getTimestamp() + ")");
 
 			}
 
 			setMsgReceived(new ArrayList<Message>());
 
-		} else  {
+		} else {
 
-			Network network = (Network)FindProjection("GlobalMessenger/SensorNetwork");
-			Iterator netNeighbors = new NetworkAdjacent(network, this).query().iterator();
+			Network network = (Network) FindProjection("GlobalMessenger/SensorNetwork");
+			Iterator netNeighbors = new NetworkAdjacent(network, this).query()
+					.iterator();
 
 			while (netNeighbors.hasNext()) {
-				Node aNetNeighbor = (Node)netNeighbors.next();
-				returnValue.add(new Message(this, aNetNeighbor, new String("Hello from "+this+" to "+ aNetNeighbor), timeDouble));
+				Node aNetNeighbor = (Node) netNeighbors.next();
+				returnValue.add(new Message(this, aNetNeighbor, new String(
+						"Hello from " + this + " to " + aNetNeighbor),
+						timeDouble));
 			}
-
 
 		}
 
@@ -263,10 +283,11 @@ public class Node  {
 	}
 
 	/**
-	 *
+	 * 
 	 * This method provides a human-readable name for the agent.
+	 * 
 	 * @method toString
-	 *
+	 * 
 	 */
 	@ProbeID()
 	public String toString() {
@@ -278,6 +299,4 @@ public class Node  {
 
 	}
 
-
 }
-
