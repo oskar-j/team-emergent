724d68a2b943c9d7615095b74a089159ff94913c
Oskar Jarczyk <oskar.jarczyk@gmail.com>
2013-06-25 15:57:55 +0200

    CMM.java

---
+++ src/EmergingTeams/CMM.java
@@ -0,0 +1,36 @@
+package EmergingTeams;
+
+public class CMM {
+
+	public enum level {
+		INITIAL, REPEATABLE, DEFINED, MANAGED, OPTIMIZING
+	};
+
+	private int stage;
+
+	public CMM() {
+
+	}
+
+	public String toString() {
+		switch (stage) {
+		case 1: {
+			return "Initial";
+		}
+		case 2: {
+			return "Repeatable";
+		}
+		case 3: {
+			return "Defined";
+		}
+		case 4: {
+			return "Managed";
+		}
+		case 5: {
+			return "Optimizing";
+		}
+		}
+		return "ERR";
+	}
+
+}
