c4b0c1fc68a429485e3799da75b47e65a8c5ca1b
Oskar Jarczyk <oskar.jarczyk@gmail.com>
2013-06-26 13:43:19 +0200

    GlobalMessenger.java

--- src/EmergingTeams/GlobalMessenger.java
+++ src/EmergingTeams/GlobalMessenger.java
@@ -1,4 +1,3 @@
-
 package EmergingTeams;
 
 import java.io.*;
@@ -42,195 +41,213 @@ import static java.lang.Math.*;
 import static repast.simphony.essentials.RepastEssentials.*;
 
 /**
- *
+ * 
  * This is an agent.
  * 
  * @author Oskar Jarczyk
  * 
- * NB: you need to specify the correct numNodes in EmergingTeams.rs/context.html
- * otherwise the message buffer will throw an outOfBound exception.
- * If you dont know the total number of Nodes in your simulation 
- * then use an upper bound (hopefully "strict" upperbound).
- * (Changing the numNodes parameter in the GUI interface will not work!) 
- *
+ *         NB: you need to specify the correct numNodes in
+ *         EmergingTeams.rs/context.html otherwise the message buffer will throw
+ *         an outOfBound exception. If you dont know the total number of Nodes
+ *         in your simulation then use an upper bound (hopefully "strict"
+ *         upperbound). (Changing the numNodes parameter in the GUI interface
+ *         will not work!)
+ * 
  */
-public class GlobalMessenger  {
+public class GlobalMessenger {
 
-	public ArrayList<Message> queue[]; // Each element contains an ArrayList of Messages for receiver i
+	public ArrayList<Message> queue[]; // Each element contains an ArrayList of
+										// Messages for receiver i
 	public int msgSent = 0;
 	public int msgSentBefore = 0;
 	public int msgExchanged = 0;
 
-	@Parameter (displayName = "Msg Queue", usageName = "queue")
-	public synchronized ArrayList<Message>[] getQueue() { return queue; }
-	public synchronized void setQueue(ArrayList<Message>[] newValue) { queue = newValue;  }
+	@Parameter(displayName = "Msg Queue", usageName = "queue")
+	public synchronized ArrayList<Message>[] getQueue() {
+		return queue;
+	}
 
-	@Parameter (displayName = "Msg Sent Counter", usageName = "msgSent")
-	public synchronized int getMsgSent() { return msgSent; }
-	public synchronized void setMsgSent(int newValue) { msgSent = newValue; }
+	public synchronized void setQueue(ArrayList<Message>[] newValue) {
+		queue = newValue;
+	}
 
+	@Parameter(displayName = "Msg Sent Counter", usageName = "msgSent")
+	public synchronized int getMsgSent() {
+		return msgSent;
+	}
 
-	@Parameter (displayName = "MsgSentBefore", usageName = "msgSentBefore")
-	public synchronized int getMsgSentBefore() { return msgSentBefore; }
-	public synchronized void setMsgSentBefore(int newValue) { msgSentBefore = newValue; }
+	public synchronized void setMsgSent(int newValue) {
+		msgSent = newValue;
+	}
 
+	@Parameter(displayName = "MsgSentBefore", usageName = "msgSentBefore")
+	public synchronized int getMsgSentBefore() {
+		return msgSentBefore;
+	}
 
-	@Parameter (displayName = "msgExchanged", usageName = "msgExchanged")
-	public synchronized int getMsgExchanged() { return msgExchanged; }
-	public synchronized void setMsgExchanged(int newValue) { msgExchanged = newValue; }
+	public synchronized void setMsgSentBefore(int newValue) {
+		msgSentBefore = newValue;
+	}
 
+	@Parameter(displayName = "msgExchanged", usageName = "msgExchanged")
+	public synchronized int getMsgExchanged() {
+		return msgExchanged;
+	}
 
-
+	public synchronized void setMsgExchanged(int newValue) {
+		msgExchanged = newValue;
+	}
 
 	/**
-	 *
+	 * 
 	 * This value is used to automatically generate agent identifiers.
+	 * 
 	 * @field serialVersionUID
-	 *
+	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
 	/**
-	 *
+	 * 
 	 * This value is used to automatically generate agent identifiers.
+	 * 
 	 * @field agentIDCounter
-	 *
+	 * 
 	 */
 	protected static long agentIDCounter = 1;
 
 	/**
-	 *
+	 * 
 	 * This value is the agent's identifier.
+	 * 
 	 * @field agentID
-	 *
+	 * 
 	 */
 	protected String agentID = "GlobalMessenger " + (agentIDCounter++);
 
 	/**
-	 *
+	 * 
 	 * This is the step behavior.
+	 * 
 	 * @method initialize
-	 *
+	 * 
 	 */
 	public void initialize() {
-		
-		Parameters p = RunEnvironment.getInstance().getParameters();
-		int numNodes = (Integer)p.getValue("numNodes");
 
-		queue = new ArrayList[numNodes+1];
+		Parameters p = RunEnvironment.getInstance().getParameters();
+		int numNodes = (Integer) p.getValue("numNodes");
+
+		queue = new ArrayList[numNodes + 1];
 		// Note that receiver i = 0 is not used
 		// (Ths Node class creates agentIDs starting by 1)
-		for(int i = 0; i < queue.length; i++) {
-			queue[i] = new ArrayList<Message>();			
+		for (int i = 0; i < queue.length; i++) {
+			queue[i] = new ArrayList<Message>();
 		}
 
-		//It's always useful to know which day/time it is
-		//(especially if you're a timetraveller!)
-		Date todaysDate = new java.util.Date();
-		SimpleDateFormat formatter = new SimpleDateFormat("dd-MMM-yyyy_HH:mm:ss");
-		String formattedDate = formatter.format(todaysDate);
-		System.out.println("Today’s date and Time is: "+formattedDate);
-		System.out.println("(Useful in case you're a timetraveller)");
+		say("Today's date and Time is: " + formattdate());
+		say("(Useful in case you're a time traveller)");
 
 	}
 
+	@SuppressWarnings("unused")
+	private void say(String s) {
+		System.out.println(s);
+	}
+
+	private String formattdate() {
+		// It's always useful to know which day/time it is
+		// (especially if you're a time traveller!)
+		Date todaysDate = new java.util.Date();
+		SimpleDateFormat formatter = new SimpleDateFormat(
+				"dd-MMM-yyyy_HH:mm:ss");
+		return formatter.format(todaysDate);
+	}
+
 	/**
-	 *
+	 * 
 	 * This is the step behavior.
+	 * 
 	 * @method send
-	 *
+	 * 
 	 */
 	public synchronized void send(Message aMsg) {
-	
-		int numericReceiverID = (int)aMsg.receiver.numericID; //DANGER unsafe conversion long to int
+
+		int numericReceiverID = (int) aMsg.receiver.numericID; 
+		// DANGER unsafe conversion long to int
 		queue[numericReceiverID].add(aMsg);
 		msgSent++;
 
 	}
 
 	/**
-	 *
+	 * 
 	 * This is the step behavior.
+	 * 
 	 * @method receive
 	 * 
-	 * This method delivers the message to the recipient at the next time step.
-	 * If you want to simulate multihop message passing and traffic congestion,
-	 * this might be the right place to do it.
-	 * E.g.: implement a mechanism to wait to deliver the message after a number of timesteps
-	 * equals to the minimum number of hops among two node. You also need to 
-	 * increment the counter of messages exchanged inside the GlobalMessenger object.
+	 *         This method delivers the message to the recipient at the next
+	 *         time step. If you want to simulate multihop message passing and
+	 *         traffic congestion, this might be the right place to do it. E.g.:
+	 *         implement a mechanism to wait to deliver the message after a
+	 *         number of timesteps equals to the minimum number of hops among
+	 *         two node. You also need to increment the counter of messages
+	 *         exchanged inside the GlobalMessenger object.
 	 * 
-	 *  Note that another more correct way of implementing multihop is to leave the 
-	 *  GlobalMessenger as is, since it was designed for exchanging messages one-to-one
-	 *  and then embed on the node a routing mechanism.
-	 *
+	 *         Note that another more correct way of implementing multihop is to
+	 *         leave the GlobalMessenger as is, since it was designed for
+	 *         exchanging messages one-to-one and then embed on the node a
+	 *         routing mechanism.
+	 * 
 	 */
+	@SuppressWarnings("rawtypes")
 	public synchronized ArrayList<Message> receive(Node receiverNode) {
-		
-		
 		ArrayList<Message> msgsForReceiver = new ArrayList<Message>();
-		Iterator queueIterator = queue[(int)receiverNode.numericID].iterator();//DANGER unsafe conversion long to int
-		
+		Iterator queueIterator = queue[(int) receiverNode.numericID].iterator();
+		// DANGER unsafe conversion long to int
 		while (queueIterator.hasNext()) {
-
-			Message aMessage = (Message)queueIterator.next();
-
+			Message aMessage = (Message) queueIterator.next();
 			if (aMessage.timestamp < GetTickCount()) {
 				msgsForReceiver.add(aMessage);
 				queueIterator.remove();
-			} 
-		}		
-		
+			}
+		}
 		return msgsForReceiver;
 	}
-	
 
 	/**
-	 *
+	 * 
 	 * This is the step behavior.
-	 * @method step
-	 * here you can check your variables for statistics.
-	 *  
+	 * 
+	 * @method step here you can check your variables for statistics.
+	 * 
 	 */
-	@ScheduledMethod(
-			start = 1d,
-			interval = 1d,
-			priority = -1.7976931348623157E308d, //priority LAST
-			shuffle = false
-	)
+	@ScheduledMethod(start = 1d, interval = 1d, 
+			priority = -1.7976931348623157E308d, // priority LAST
+	shuffle = false)
 	public synchronized void step() {
-
-		System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
-		System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+		System.out
+				.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+		System.out
+				.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
 		double timeDouble = GetTickCount();
 		msgExchanged = msgSent - msgSentBefore;
-		System.out.println(timeDouble+" "+this+" msgExchanged = "+msgExchanged);
+		System.out.println(timeDouble + " " + this + " msgExchanged = "
+				+ msgExchanged);
 		msgSentBefore = msgSent;
-
-
 	}
 
-
-
-
-
-
 	/**
-	 *
+	 * 
 	 * This method provides a human-readable name for the agent.
+	 * 
 	 * @method toString
-	 *
+	 * 
 	 */
 	@ProbeID()
 	public String toString() {
-
 		// Set the default agent identifier.
-		String returnValue = this.agentID;		
+		String returnValue = this.agentID;
 		return returnValue;
-
 	}
 
-
 }
-
-
